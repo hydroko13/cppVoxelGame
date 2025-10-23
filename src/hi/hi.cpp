0afeb159f36fb41431dc1225b0c7e7915736170b
Maxim Kozlov <maximko.ca@gmail.com>
2025-10-23 07:01:31 -0400

    Basic window code

--- src/hi/hi.cpp
+++
@@ -1,7 +0,0 @@
-#include "hi.h"
-
-
-
-void sayHello() {
-    std::cout << "Hello" << std::endl;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include "hi/hi.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 
 int main() {
 
 
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My title", NULL, NULL);
 
-    sayHello();
+    if (window == NULL) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+
+
+
 
     return 0;
 }
